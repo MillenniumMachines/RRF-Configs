b307c1540baf6560bca6e4e426ee297b5b7f8c8b
Ben Agricola <ben+git@agrico.la>
2024-05-18 10:34:05 +0100

    Put upgrade notes above release contents
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- dist/release-all.sh
+++ dist/release-all.sh
@@ -20,6 +20,15 @@ rm -f "${RNOTES_PATH}"
 cat <<-EOF >>"${RNOTES_PATH}"
 	# Release ${COMMIT_ID}
 
+	## Upgrading
+
+	* You can upload any of these release zip files from Duet Web Control (DWC), via the "Files -> System" link in the menu.
+	* **NOTE**: If you upload the file via DWC and click 'Yes' to upgrade, the WiFi module will be flashed twice. There is currently no way around this, we need to do this to support extracting the file directly to the SD card for initial installations.
+	* The first time your machine reboots after installing the new release, it will switch back into Access Point mode and will _not_ connect to your WiFi network if it was configured to do so - this is because WiFi network details might be wiped when the WiFi module is updated, and bringing the board back up in AP mode allows recovery without having to connect over USB.
+	* You can connect to the access point using the password in the [documentation](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#accessing-duet-web-control), and check if your WiFi network details need to be re-added using [M587](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#configure-your-wifi-network).
+	* You may then reboot, and the machine will attempt to connect to your network again.
+	* Please see below for details of what is included in each release.
+
 	## Milo V1.5
 
 EOF
@@ -33,15 +42,5 @@ build_release
 
 load_release "milo-v1.5" "reference-skr3-ez-5160"
 build_release
-
-cat <<-EOF >>"${RNOTES_PATH}"
-	## Upgrading
-
-	* You can upload any of these release zip files from Duet Web Control (DWC), via the "Files -> System" link in the menu.
-	* **NOTE**: If you upload the file via DWC and click 'Yes' to upgrade, the WiFi module will be flashed twice. There is currently no way around this, we need to do this to support extracting the file directly to the SD card for initial installations.
-	* The first time your machine reboots after installing the new release, it will switch back into Access Point mode and will _not_ connect to your WiFi network if it was configured to do so - this is because WiFi network details might be wiped when the WiFi module is updated, and bringing the board back up in AP mode allows recovery without having to connect over USB.
-	* You can connect to the access point using the password in the [documentation](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#accessing-duet-web-control), and check if your WiFi network details need to be re-added using [M587](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#configure-your-wifi-network).
-	* You may then reboot, and the machine will attempt to connect to your network again.
-EOF
 
 clean_cache_dir
