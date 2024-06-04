2d8a04119f09a21470b150ade611b9c46b299948
Ben Agricola <ben+git@agrico.la>
2024-06-04 12:55:25 +0100

    Make release process quieter
    Also make sure we evaluate `COMMIT_ID` properly.
    
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- dist/release-all.sh
+++ dist/release-all.sh
@@ -17,7 +17,7 @@ ENABLE_RNOTES="aww yiss"
 
 # Add release notes header
 rm -f "${RNOTES_PATH}"
-cat <<-"EOF" >>"${RNOTES_PATH}"
+cat <<-EOF >>"${RNOTES_PATH}"
 	# Release ${COMMIT_ID}
 
 	## Upgrading
@@ -26,7 +26,7 @@ cat <<-"EOF" >>"${RNOTES_PATH}"
 	* **NOTE**: If you upload the file via DWC and click 'Yes' to upgrade, the WiFi module will be flashed twice. There is currently no way around this, we need to do this to support extracting the file directly to the SD card for initial installations.
 	* The first time your machine reboots after installing the new release, it will switch back into Access Point mode and will _not_ connect to your WiFi network if it was configured to do so - this is because WiFi network details might be wiped when the WiFi module is updated, and bringing the board back up in AP mode allows recovery without having to connect over USB.
 	* You can connect to the access point using the password in the [documentation](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#accessing-duet-web-control), and check if your WiFi network details need to be re-added using [M587](https://millenniummachines.github.io/docs/milo/manual/chapters/90_install_rrf/#configure-your-wifi-network).
-	* You may then reboot, and the machine will revert to the existing configuration in `network-default.g` or `network.g` (if you have one).
+	* You may then reboot, and the machine will revert to the existing configuration in \`network-default.g\` or \`network.g\` (if you have one).
 	* Please see below for details of what is included in each release.
 
 	## Milo V1.5
